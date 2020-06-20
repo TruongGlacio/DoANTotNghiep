930a71a62d1a37e5e465f6b8864fae1b8614b25a
TruongGlacio <buixuantruong.tb@gmail.com>
2020-06-20 10:47:35 +0700

    Creat branch

--- EyeSleepDetect/eyesleepdetectclass.cpp
+++ EyeSleepDetect/eyesleepdetectclass.cpp
@@ -9,7 +9,11 @@
 #include"opencv2/videoio.hpp"
 EyeSleepDetectClass::EyeSleepDetectClass(QObject *parent) : QObject(parent)
 {
-    StartDetectEyeSleep();
+    connect(this, SIGNAL(GetCvImage(std::vector<dlib::rectangle>)), this, SLOT(SetCvImage(std::vector<dlib::rectangle>)));
+    connect(this, SIGNAL(GetShape_predictor(shape_predictor)), this, SLOT(SetShape_predictor(shape_predictor)));
+    moveToThread(&thread);
+    connect(&thread, SIGNAL(started()), this, SLOT(DeserializeFace())); //cant have parameter sorry, when using connect
+    thread.start();
 }
 double EyeSleepDetectClass::compute_EAR(std::vector<cv::Point> vec)
 {
@@ -21,8 +25,18 @@ double EyeSleepDetectClass::compute_EAR(std::vector<cv::Point> vec)
     double ear = (a + b) / (2.0 * c);
     return ear;
 }
+void EyeSleepDetectClass::DeserializeFace()
+{
+    shape_predictor sp;
+    cv_image<bgr_pixel> cimg;
+    frontal_face_detector detector = get_frontal_face_detector();
+    deserialize(SHAPE_PREDIRTOR_68_FACE_LANDMARK)>> sp;
+    std::vector<dlib::rectangle> faces = detector(cimg);
+    emit GetCvImage(faces);
+    emit GetShape_predictor(sp);
 
-int EyeSleepDetectClass:: StartDetectEyeSleep()
+}
+int EyeSleepDetectClass::StartDetectEyeSleep()
 {
     try {
         cv::VideoCapture cap(0);
@@ -35,9 +49,8 @@ int EyeSleepDetectClass:: StartDetectEyeSleep()
             return 1;
         }
         // Load face detection and deserialize  face landmarks model.
-        frontal_face_detector detector = get_frontal_face_detector();
-
-        deserialize(SHAPE_PREDIRTOR_68_FACE_LANDMARK) >> sp;
+       // frontal_face_detector detector = get_frontal_face_detector();
+        //deserialize(SHAPE_PREDIRTOR_68_FACE_LANDMARK)>> sp;
 
         // Grab and process frames until the main window is closed by the user.
         while (!win.is_closed()) {
@@ -47,60 +60,83 @@ int EyeSleepDetectClass:: StartDetectEyeSleep()
                 break;
             }
 
-            cv_image<bgr_pixel> cimg(temp);
+            //cv_image<bgr_pixel> cimg(temp);
+            mCimg=cv_image<bgr_pixel>(temp);
             full_object_detection shape;
-
             // Detect faces
-            std::vector<dlib::rectangle> faces = detector(cimg);
-            qDebug()<< "Number of faces detected: " << faces.size() << endl;
-
-            win.clear_overlay();
-            win.set_image(cimg);
-
-            // Find the pose of each face.
-            if (faces.size() > 0) {
-
-                shape = sp(cimg, faces[0]); //work only with 1 face
-
-                for (int b = 36; b < 42; ++b) {
-                    p.x = shape.part(b).x();
-                    p.y = shape.part(b).y();
-                    lefteye.push_back(p);
-                }
-                for (int b = 42; b < 48; ++b) {
-                    p.x = shape.part(b).x();
-                    p.y = shape.part(b).y();
-                    righteye.push_back(p);
-                }
-                //Compute Eye aspect ration for eyes
-                double right_ear = compute_EAR(righteye);
-                double left_ear = compute_EAR(lefteye);
-
-                //if the avarage eye aspect ratio of lef and right eye less than 0.2, the status is sleeping.
-                if ((right_ear + left_ear) / 2 < 0.2)
-                    win.add_overlay(dlib::image_window::overlay_rect(faces[0], rgb_pixel(255, 255, 255), "Sleeping"));
-                else
-                    win.add_overlay(dlib::image_window::overlay_rect(faces[0], rgb_pixel(255, 255, 255), "Not sleeping"));
-
-                righteye.clear();
-                lefteye.clear();
-
-                win.add_overlay(render_face_detections(shape));
-
-                c = (char)waitKey(30);
-                if (c == 27)
-                    break;
-            }
+            //std::vector<dlib::rectangle> faces; = detector(cimg);
+            //qDebug()<< "Number of faces detected: " << faces.size() << endl;
+            //DrawFaceAfterDetect(&win,&mCimg,&mFaces,mSp);
+            c = (char)waitKey(50);
+            if (c == 27)
+                break;
         }
     }
     catch (serialization_error& e) {
         cout << "Check the path to dlib's default face landmarking model file to run this example." << endl;
         cout << "You can get it from the following URL: " << endl;
-        cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
+        cout << "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
         cout << endl
              << e.what() << endl;
     }
     catch (exception& e) {
         cout << e.what() << endl;
     }
+    return 0;
 }
+
+void EyeSleepDetectClass::SetShape_predictor(shape_predictor sp)
+{
+    mSp=sp;
+}
+
+void EyeSleepDetectClass::SetCvImage(std::vector<dlib::rectangle> faces)
+{
+    mFaces=faces;
+}
+void EyeSleepDetectClass::DrawFaceAfterDetect(image_window *mwin,cv_image<bgr_pixel> *cimg,std::vector<dlib::rectangle> *faces,  shape_predictor sp)
+{
+    full_object_detection shape;
+    std::vector<cv::Point> _righteye;
+    std::vector<cv::Point> _lefteye;
+    cv::Point p;
+    char c;
+
+
+    mwin->clear_overlay();
+    mwin->set_image(*cimg);
+
+    // Find the pose of each face.
+    if (faces->size() > 0) {
+
+        shape = sp(*cimg, faces->at(0)); //work only with 1 face
+
+        for (int b = 36; b < 42; ++b) {
+            p.x = shape.part(b).x();
+            p.y = shape.part(b).y();
+            _lefteye.push_back(p);
+        }
+        for (int b = 42; b < 48; ++b) {
+            p.x = shape.part(b).x();
+            p.y = shape.part(b).y();
+            _righteye.push_back(p);
+        }
+        //Compute Eye aspect ration for eyes
+        double right_ear = compute_EAR(_righteye);
+        double left_ear = compute_EAR(_lefteye);
+
+        //if the avarage eye aspect ratio of lef and right eye less than 0.2, the status is sleeping.
+        if ((right_ear + left_ear) / 2 < 0.2)
+            mwin->add_overlay(dlib::image_window::overlay_rect(faces->at(0), rgb_pixel(255, 255, 255), "Sleeping"));
+        else
+            mwin->add_overlay(dlib::image_window::overlay_rect(faces->at(0), rgb_pixel(255, 255, 255), "Not sleeping"));
+
+        _righteye.clear();
+        _lefteye.clear();
+
+        mwin->add_overlay(render_face_detections(shape));
+
+    }
+}
+
+
--- EyeSleepDetect/eyesleepdetectclass.h
+++ EyeSleepDetect/eyesleepdetectclass.h
@@ -3,7 +3,7 @@
 
 #define CAM_WIDTH 640
 #define CAM_HEIGHT 480
-#define SHAPE_PREDIRTOR_68_FACE_LANDMARK "D:\Projects\GitProjects\DoAnTotNghiep\DoANTotNghiep\dlib\shape_predictor_68_face_landmarks.dat"
+#define SHAPE_PREDIRTOR_68_FACE_LANDMARK "E:\\GitProiects\\DoANTotNghiep\\dlib\\shape_predictor_68_face_landmarks.dat"
 #include <QObject>
 #include <dlib/opencv.h>
 #include <opencv2/highgui/highgui.hpp>
@@ -12,8 +12,10 @@
 #include <dlib/image_processing.h>
 #include <dlib/gui_widgets.h>
 #include <dlib/image_io.h>
+#include<QTimer>
 //#include "opencv2/opencv.hpp"
 //#include"opencv2/videoio.hpp"
+#include<QThread>
 
 using namespace dlib;
 using namespace std;
@@ -26,17 +28,28 @@ class EyeSleepDetectClass : public QObject
 public:
     explicit EyeSleepDetectClass(QObject *parent = nullptr);
     double compute_EAR(std::vector<cv::Point> vec);
+    void DrawFaceAfterDetect(image_window *mwin,cv_image<bgr_pixel> *cimg,std::vector<dlib::rectangle> *faces,  shape_predictor sp);
     int StartDetectEyeSleep();
 signals:
+   void GetCvImage(std::vector<dlib::rectangle> faces);
+   void GetShape_predictor(shape_predictor sp);
+public slots:
+   void  DeserializeFace();
+   void SetCvImage(std::vector<dlib::rectangle> faces);
+   void SetShape_predictor(shape_predictor sp);
+public:
+   cv_image<bgr_pixel> mCimg;
+
 private:
   cv::VideoCapture* m_videoCapture;
   image_window win;
-  shape_predictor sp;
+  shape_predictor mSp;
+  std::vector<dlib::rectangle> mFaces;
   std::vector<cv::Point> righteye;
   std::vector<cv::Point> lefteye;
   char c;
   cv::Point p;
-
+  QThread thread;
 };
 
 #endif // EYESLEEPDETECTCLASS_H
