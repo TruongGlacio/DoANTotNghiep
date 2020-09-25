316f0a8a0985444fdf32f2d2218133bfbddc81eb
TruongGlacio <buixuantruong.tb@gmail.com>
2020-09-26 00:00:30 +0700

    Update project

--- BrainDetect/BrainDetectFunction.py
+++ BrainDetect/BrainDetectFunction.py
@@ -447,7 +447,7 @@ class BrainDetectFunction:
         global model
         physical_devices = tf.config.experimental.list_physical_devices('GPU')
         if len(physical_devices) > 0:
-           tf.config.experimental.set_memory_growth(physical_devices[0], True)              
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)              
         IMG_WIDTH = IMAGE_SIZE_FOR_BUILDMODEL
         IMG_HEIGHT =IMAGE_SIZE_FOR_BUILDMODEL
           
