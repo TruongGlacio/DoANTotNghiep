291d67fce1661b55269072e2fe1ceb03a146ce42
TruongGlacio <buixuantruong.tb@gmail.com>
2020-11-12 23:38:11 +0700

    Delete handDetectControlApp folder

--- HandDetectControlApp/ConnectLayer/StringDefines.h
+++
@@ -1,6 +0,0 @@
-#ifndef STRINGDEFINES_H
-#define STRINGDEFINES_H
-#include <QString>
-const QString TEMP_IMAGE_FILE_PATH="E:\\GitProiects\\DoANTotNghiep\\HandDetectControlApp\\ObjectSampleSources\\Hands\\TEMP\\";
-#endif // STRINGDEFINES_H
-
--- HandDetectControlApp/main.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick 2.1
-import QtQuick.Window 2.1
-
-Window {
-    visible: true
-    width: 1366
-    height: 768
-    title: qsTr("Hello World")
-}
