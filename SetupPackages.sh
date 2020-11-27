e8f68a93723e3a173aafa60aedb0cbf7652ee620
TruongGlacio <buixuantruong.tb@gmail.com>
2020-11-28 00:52:52 +0700

    Update Setup packagefile

--- SetupPackages.sh
+++ SetupPackages.sh
@@ -1,3 +1,3 @@
-sudo apt-get install mingw-w64 libgtk2.0-dev libgtk3.0-cil-dev libcanberra-gtk3-module mesa-common-dev build-essential libfontconfig1 libpng-dev
+sudo apt-get install mingw-w64 libgtk2.0-dev libgtk3.0-cil-dev libcanberra-gtk3-module mesa-common-dev build-essential libfontconfig1 libpng-dev libcanberra-gtk-module:i386
 
 #gcc-mingw-w64 libgtk2.0-dev libcanberra-gtk-module
